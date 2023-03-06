418f016e734254c62097680706a7ec714edfd222
sycodes95 <sycodes95@gmail.com>
2023-03-06 00:17:32 -0800

    skeleton set up complete with dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <div className="">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'steel-blue': '#3581B8',
+        'macaroni' : '#FCB07E',
+        'alabaster' : '#DEE2D6',
+        'jet': '#323031',
+        'ruby': '#8B2635',
+        'gunmetal' : '#2B303A',
+        'celadon' : '#A9E5BB',
+        'winter-green' : '#638475',
+        'desert' : '#F6D0B1',
+        'orange-web' : '#FCA311',
+        'oxford-blue' : '#14213D',
+        'aquamarine' : "#3DDC97",
+        'raisin' : '#211A1E',
+        'red-orange' : '#D84727'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}

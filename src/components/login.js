8e948bf84e28d79c66317f749624f7a9e5e4fc8d
sycodes95 <sycodes95@gmail.com>
2023-03-07 00:37:35 -0800

    login page finished, connected to api

--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 function Login () {
 
+  const [loginError, setLoginError] = useState(null)
+
   const [formData, setFormData] = useState({
     email: "",
     password: ""
@@ -13,14 +15,35 @@ function Login () {
     setFormData({...formData, [name]: value})
   }
 
-  const handleLogIn = () =>{
-
+  const handleLogIn = (e) =>{
+    e.preventDefault()
+    const host = process.env.REACT_APP_API_HOST
+    fetch(`${host}/users/log-in-post`, {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: { 'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(data => {
+      
+      const token = data.token;
+      if(token) {
+        localStorage.setItem('token', JSON.stringify(token))
+        window.location.href = '/'
+      } else {
+        setLoginError('Incorrect email or password')
+      }
+      
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   }
   return(
     <div className="LOG-IN-CONTAINER h-fit flex flex-1 flex-col items-center pb-16 ">
       
       <form className="LOG-IN-FORM mt-8  flex flex-col gap-y-2 items-center p-4 rounded-md
-      text-black text-sm bg-black bg-opacity-25 pb-8" onSubmit={handleLogIn}>
+      text-black text-sm bg-black bg-opacity-25 pb-8">
         <div className="h-48 w-48 bg-earth">
         </div>
         <div className="h-12 w-full rounded-sm flex justify-center items-center text-white text-2xl ">AUTHORIZATION</div>
@@ -28,14 +51,14 @@ function Login () {
         
         <label className="text-white font-black-outline">Email</label>
         <input className=" w-full h-10 rounded-sm p-1 border border-black bg-black bg-opacity-25 text-white caret-white text-xl" name="email" type="email" value={formData.email} onChange={handleInputChange}/>
-        <div className="h-4 text-red-600"></div>
+        <div className="h-4 text-red-600">{loginError}</div>
 
         <label className="text-white font-black-outline">Password</label>
         <input className="w-full h-10 rounded-sm p-1 border border-black bg-black bg-opacity-25 text-white caret-white text-xl" name="password" type="password" value={formData.password} onChange={handleInputChange}/>
         <div className="h-4 text-red-600"></div>
 
-        <button className="w-full h-10  text-white text-xl bg-green-800 bg-opacity-25 hover:bg-green-700 hover:bg-opacity-25 transition-colors font-black-outline" type="submit">LOG IN</button>
-        <button className="w-full h-6 mt-4 text-white bg-red-800 bg-opacity-25 hover:bg-red-700 hover:bg-opacity-25 transition-colors font-black-outline text-xs" type="submit">USE DEMO ACCOUNT</button>
+        <button className="w-full h-10  text-white text-xl bg-green-800 bg-opacity-25 hover:bg-green-700 hover:bg-opacity-25 transition-colors font-black-outline" onClick={handleLogIn}>LOG IN</button>
+        <button className="w-full h-6 mt-4 text-white bg-red-800 bg-opacity-25 hover:bg-red-700 hover:bg-opacity-25 transition-colors font-black-outline text-xs" onClick={handleLogIn}>USE DEMO ACCOUNT</button>
       </form>
     </div>
   )
